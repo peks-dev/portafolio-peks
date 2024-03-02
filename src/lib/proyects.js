5968eafc0218e24a6cf95f9e0b476dd4bacb1b92
Peks <0peks0@proton.me>
2024-03-02 01:47:34 -0600

    home ready, icons jsx created

---
+++ src/lib/proyects.js
@@ -0,0 +1,53 @@
+export const projects = [
+  {
+    name: "basket places",
+    liveSite: "https://basket-places.netlify.app/",
+    repository: "https://github.com/peks-dev/basket-places",
+    coverImg: "/assets/webs/basket-places.jpeg",
+    tecnologys: ["next", "react", "supabase"],
+  },
+  {
+    name: "aguilar talleres",
+    liveSite: "https://aguilartalleres.com/",
+    repository: "https://github.com/peks-dev/new-AT",
+    coverImg: "/assets/webs/aguilar-talleres.jpeg",
+    tecnologys: ["astro"],
+  },
+  {
+    name: "sign rite",
+    liveSite: "https://sign-rite.com/",
+    repository: "",
+    coverImg: "/assets/webs/sign-rite.jpeg",
+    tecnologys: ["wordpress", "divi"],
+  },
+  {
+    name: "julian DP",
+    liveSite: "https://juliancollazodp.com/",
+    repository: "https://github.com/peks-dev/julian-astro-web",
+    coverImg: "/assets/webs/julian.webp",
+    tecnologys: ["astro"],
+  },
+  {
+    name: "dentist",
+    liveSite: "https://dental-acosta.netlify.app/",
+    repository: "https://github.com/peks-dev/dentist-website",
+    coverImg: "/assets/webs/dental-acosta.jpg",
+    tecnologys: ["astro"],
+  },
+
+  {
+    name: "cbm",
+    liveSite: "https://clinica-de-baloncesto-mexicano.netlify.app/",
+    repository: "https://github.com/peks-dev/CBM-web",
+    coverImg: "/assets/webs/cbm.jpg",
+    tecnologys: ["html", "css", "javascript"],
+  },
+
+  {
+    name: "guerreros",
+    liveSite: "https://club-guerreros.netlify.app/",
+    repository: "https://github.com/peks-dev/guerreros-tizimin",
+    coverImg: "/assets/webs/guerreros.jpeg",
+    tecnologys: ["html", "css", "javascript"],
+  },
+];
